1fd27955cd5ed428a264276b2b9f013cf56eca25
nmsadoques17 <32141075+nmsadoques17@users.noreply.github.com>
2017-10-19 11:47:55 -0400

    inferior ai

--- twentyonegame.js
+++ twentyonegame.js
@@ -8,6 +8,7 @@ do { // end at 21
 	// call continuing function for user
 	player = "you";
 	steps = prompt("Starting at "+count+", how may steps do you count(1-3) ");
+	count = counting(player,count,steps);
 	// call counting function for computer, at 2 steps each time
 	player = "me";
 	steps = 2; // temporary - computer always counts 2 steps
@@ -19,3 +20,12 @@ alert ("You win"); // temporary
 
 
 //count and display function
+function counting(player,current,steps) {
+	game=[];
+	for (index=0;index<steps;index++){
+		game[index]=current+index+1;
+	}
+	alert(player+": "+game);
+	current=parseInt(current)+parseInt(steps);
+	return current;
+}
