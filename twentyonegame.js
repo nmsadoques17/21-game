3614291d2736c643446f3c5207279d95681aff85
nmsadoques17 <32141075+nmsadoques17@users.noreply.github.com>
2017-10-19 11:47:12 -0400

    inferior computer

---
+++ twentyonegame.js
@@ -0,0 +1,21 @@
+//initialize variables
+var count = 0; // where the game is
+var steps = 0; // 1, 2 or 3
+var game = []; //numbers counted
+var player = ""; // person whose turn it is
+
+do { // end at 21
+	// call continuing function for user
+	player = "you";
+	steps = prompt("Starting at "+count+", how may steps do you count(1-3) ");
+	// call counting function for computer, at 2 steps each time
+	player = "me";
+	steps = 2; // temporary - computer always counts 2 steps
+	count = counting(player,count,steps;
+}
+while (count<21);
+
+alert ("You win"); // temporary
+
+
+//count and display function
